0745ad329ea4171e5c398fd4d176753f684cb32c
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-05-03 08:16:05 +0700

    call api and get or create user

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+const { username } = req.body;
+  try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "32d1e300-9873-4392-84ec-2afef81e0bab" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch(e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
 app.listen(3001);
